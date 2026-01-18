9061870d05cce3ed7967d9fee890870ff93dc159
Hudisson R Xavier <hudissonxavier2018@gmail.com>
2026-01-18 08:08:46 -0300

    Add classe RoutineScheduler que executa uma verificação a cada minuto para processar as filas de envio

---
+++ src/test/java/com/hudissonxavier/avisame/AvisameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hudissonxavier.avisame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvisameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
