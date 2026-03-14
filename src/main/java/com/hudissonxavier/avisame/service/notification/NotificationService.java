983c80e81f0b9eb68743990ae988c0efe81eaea2
Hudisson R Xavier <hudissonxavier2018@gmail.com>
2026-03-14 14:39:37 -0300

    Realizado melhorias no rodapé do email

--- src/main/java/com/hudissonxavier/avisame/service/notification/NotificationService.java
+++ src/main/java/com/hudissonxavier/avisame/service/notification/NotificationService.java
@@ -15,7 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public class NotificationService {
-    
+
     private final EmailService emailService;
 
     public void processarEnvio(UserModel user, List<DailyTaskModel> tarefas, List<ScheduledEventModel> eventos) {
@@ -39,12 +39,34 @@ public class NotificationService {
         if (!eventos.isEmpty()) {
             html.append("<h3>📅 Eventos Agendados:</h3><ul>");
             eventos.forEach(e -> html.append("<li><strong>")
-                .append(e.getTitle()).append("</strong>: ")
-                .append(e.getDescription()).append("</li>"));
+                    .append(e.getTitle()).append("</strong>: ")
+                    .append(e.getDescription()).append("</li>"));
             html.append("</ul>");
         }
 
-        html.append("<br><p>Tenha um excelente dia!<br>Equipa <b>AvisaMe</b></p>");
+        html.append(
+                "<br><hr style='border:none;border-top:1px solid #ddd;margin:20px 0;'>");
+        html.append(
+                "<p style='font-family:Arial,sans-serif;font-size:13px;color:#555;text-align:center;'>");
+
+        html.append("Tenha um excelente dia!<br>");
+        html.append("Equipa <b>AvisaMe</b><br><br>");
+
+        html.append("<span style='color:#888;'>Links úteis:</span><br><br>");
+
+        html.append(
+                "<a href='https://github.com/hudisson' style='margin-right:10px;'>");
+        html.append(
+                "<img src='https://cdn-icons-png.flaticon.com/512/25/25231.png' width='24' height='24' alt='GitHub' style='vertical-align:middle;'>");
+        html.append("</a>");
+
+        html.append("<a href='https://linkedin.com/in/hudisson-xavier'>");
+        html.append(
+                "<img src='https://cdn-icons-png.flaticon.com/512/174/174857.png' width='24' height='24' alt='LinkedIn' style='vertical-align:middle;'>");
+        html.append("</a>");
+
+        html.append("</p>");
+
         return html.toString();
     }
 }
